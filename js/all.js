d00a820c4439dcb4801ef76fdf8ff6ea3aab1790
Mrhuang16810 <121812530+Mrhuang16810@users.noreply.github.com>
2024-02-08 22:43:41 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.deskTopNoneOneBtn').on('click',function(e){
+        e.preventDefault();
+        $('.content').toggleClass('showmenu1');
+    });
+});
+$(document).ready(function(){
+    $('.deskTopNoneTwoBtn').on('click',function(e){
+        e.preventDefault();
+        $('.content').toggleClass('showmenu2');
+    });
+});
+$(document).ready(function(){
+    $('.deskTopNoneThreeBtn').on('click',function(e){
+        e.preventDefault();
+        $('.content').toggleClass('showmenu3');
+    });
+});
+$(document).ready(function(){
+    $('.deskTopNoneFourBtn').on('click',function(e){
+        e.preventDefault();
+        $('.content').toggleClass('showmenu4');
+    });
+});
